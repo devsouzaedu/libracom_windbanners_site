0c3c3fbdddbea7a3e116061afdb80ec573be6878
José Eduardo <dev.souzaedu@gmail.com>
2025-03-25 14:33:40 -0300

    ufa

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-blue-900 bg-opacity-90 z-50">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
-    </div>
-  )
-} 
